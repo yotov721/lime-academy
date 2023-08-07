18477ed81f2c0d0a716c6bd2811075022dd5c220
yoto.yotov <yoto.yotov@fadata.eu>
2023-08-07 19:06:15 +0300

    Update deploy task
    Also add sepolia network and env vars for keys

--- week3/hardhat.config.ts
+++ week3/hardhat.config.ts
@@ -1,8 +1,30 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+console.log(process.env.INFURA_KEY)
+console.log(process.env.PRIVATE_KEY)
+
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  solidity: {
+    version: "0.8.19",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    // Sepolia Testnet
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_KEY}`,
+      chainId: 5,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  }
 };
 
 export default config;
