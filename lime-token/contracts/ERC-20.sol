89a36d7ea2d631dbac8cc19410d9e035db790ec8
yoto.yotov <yoto.yotov@fadata.eu>
2023-08-18 13:44:55 +0300

    Add custom erc-20 implementation

---
+++ lime-token/contracts/ERC-20.sol
@@ -0,0 +1,91 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.19;
+
+contract ERC20 {
+    uint256 private totalSupply;
+
+    string private name;
+    string private symbol;
+
+    mapping(address account => uint256) private _balances;
+
+    mapping(address account => mapping(address spender => uint256)) private _allowances;
+
+    error InsufficientFunds(address sender, uint256 balance, uint256 value);
+    error InsufficientAllowance(address spender, uint256 allowance, uint256 value);
+    error InsufficientBalance(address spender, uint256 burnValue, uint256 value);
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approve(address indexed owner, address indexed spender, uint256 value);
+
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint256 _totalSupply
+    ) {
+        name = _name;
+        symbol = _symbol;
+        totalSupply = _totalSupply;
+    }
+
+    function balanceOf(address _address) public view returns (uint256) {
+        return _balances[_address];
+    }
+
+    function transfer(address _to, uint256 value) public returns (bool) {
+        _transfer(msg.sender, _to, value);
+        return true;
+    }
+
+    function _transfer(address _from, address _to, uint256 value) internal {
+        require(_from != address(0), "Invalid Address");
+        require(_to != address(0), "Invalid Address");
+
+        uint256 senderBalance = _balances[_from];
+        if (senderBalance < value) {
+            revert InsufficientFunds(_from, senderBalance, value);
+        }
+
+        _balances[_from] = senderBalance - value;
+
+        _balances[_to] += value;
+
+        emit Transfer(_from, _to, value);
+    }
+
+    function approve(address _spender, uint256 value) public returns (bool) {
+        _allowances[msg.sender][_spender] = value;
+        emit Approve(msg.sender, _spender, value);
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 value) public virtual returns (bool) {
+        _spendAllowance(_from, msg.sender, value);
+        _transfer(_from, _to, value);
+        return true;
+    }
+
+    function _spendAllowance(address _owner, address _spender, uint256 _value) internal {
+        uint256 currAllowance = _allowances[_owner][_spender];
+        if (currAllowance < _value) {
+            revert InsufficientAllowance(_spender, currAllowance, _value);
+        }
+
+        _allowances[msg.sender][_spender] = currAllowance - _value;
+    }
+
+    function setAllowance(address _spender, uint256 _value) external returns (bool){
+        _allowances[msg.sender][_spender] = _value;
+        return true;
+    }
+
+    function burn(uint256 _value) public {
+        uint256 balance = _balances[msg.sender];
+        if (balance < _value) {
+            revert InsufficientBalance(msg.sender, _value, balance);
+        }
+        totalSupply -= _value;
+
+        _balances[msg.sender] = balance - _value;
+    }
+}
